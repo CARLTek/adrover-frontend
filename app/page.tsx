70237ca08b552c897f5e7a69b3d88ca471db0ae9
ibrarli <ibraralihaidar@gmail.com>
2025-05-27 00:17:33 -0700

    Updated Ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,29 @@
+import React from "react";
+import VerticalBarComponent from "@/components/ui/VerticalBarComponent";
+import PieChartComponent from "@/components/ui/PieChartComponent";
+import HorizontalBarComponent from "@/components/ui/HorizontalBarComponent"
+import LineChartDotComponent from "@/components/ui/LineChartDotComponent";
+import CardComponent from "@/components/ui/CardComponent";
+
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-row">
       {/*Navbar*/}
       <div></div>
       {/*Main Content*/}
-      <div className="grid grid-cols-4 grid-rows-3 gap-0">
+      <div className="grid grid-cols-4 grid-rows-3 gap-10">
         {/* Row 1: Cards */}
-        <div className="col-span-1 row-span-1">Card 1</div>
-        <div className="col-span-1 row-span-1">Card 2</div>
-        <div className="col-span-1 row-span-1">Card 3</div>
-        <div className="col-span-1 row-span-1">Card 4</div>
+        <div className="col-span-1 row-span-1"><CardComponent/></div>
+        <div className="col-span-1 row-span-1"><CardComponent/></div>
+        <div className="col-span-1 row-span-1"><CardComponent/></div>
+        <div className="col-span-1 row-span-1"><CardComponent/></div>
         {/* Row 2: Graph 1 spans 3 columns, Graph 2 spans 1 column */}
-        <div className="col-span-3 row-span-1">Graph 1</div>
-        <div className="col-span-1 row-span-1">Graph 2</div>
+        <div className="col-span-3 row-span-1"><VerticalBarComponent /></div>
+        <div className="col-span-1 row-span-1"><PieChartComponent/></div>
         {/* Row 3: Graph 3 spans 2 columns, Graph 4 spans 2 columns */}
-        <div className="col-span-2 row-span-1">Graph 3</div>
-        <div className="col-span-2 row-span-1">Graph 4</div>
+        <div className="col-span-2 row-span-1"><LineChartDotComponent/></div>
+        <div className="col-span-2 row-span-1"><HorizontalBarComponent/></div>
       </div>
     </div>
   );
